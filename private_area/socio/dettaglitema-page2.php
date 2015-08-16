db0555a4b5da10020c43f9be015364825e1c5bf1
EduBic <bic.studio@yahoo.it>
2015-08-16 18:18:53 +0200

    aggiunte pagine dettagli tema e info tappa php

---
+++ private_area/socio/dettaglitema-page2.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET["Seleziona"])){
+	require("../private_php/print_private_area.php");
+	require_file();
+
+	$conn=connection_db();
+
+	$user="Socio";
+
+	print_arHeader($user);
+	print_arMenu($user);
+
+	print_destema($conn,$_GET);
+
+	print_arfooter();
+	print_close();
+}
+else
+	echo "Ops! Qualcosa è andato storto!";
+?>
